23404f3e8f6630b67a406ee9d1d30e7f4409042d
Derrick Hammer <derrick@derrickhammer.com>
2024-07-06 02:06:14 -0400

    feat: initial version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # go-llm-bundler
-A go tool to create a LLM friendly bundle to be able to understand a golang codebase
+
+A go tool to create a LLM friendly bundle to be able to understand a golang codebase.
+
+Created by an LLM 🙃, with some fixes along the way 😉.
+
+---
+
+## Challenge to the Web
+
+1. Try to come up with better way to represent the data to reduce tokens
+2. Take this idea and port to other languages (preferably in go, 1 file binaries rule!)
+3. Create a super LLM tool for multiple languages (framework?).
+
+# Notes
+
+1. A previous iteration used pure JSON, but the LLM thought a hybrid text might be better on tokens. Not actually tested but guess we will find out 🙃.
+2. AI Code tools in IDE's I believe are doing things like this, but this isolated creates an agnostic way to tell any LLM about your project and ask questions.  I have been tinkering with this on/off for a while now.
---
+++ main.go
@@ -0,0 +1,253 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"golang.org/x/mod/modfile"
+)
+
+type BundleMeta struct {
+	D []string            `json:"d,omitempty"` // Dependencies
+	I []string            `json:"i,omitempty"` // Imports
+	S map[string][]string `json:"s,omitempty"` // Structure (package -> files)
+}
+
+type Config struct {
+	ProjectDir  string
+	OutputFile  string
+	ExcludeDirs []string
+	IncludeMeta bool
+	MinifyLevel int
+}
+
+func minifyGoCode(code string, level int) (string, error) {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", code, parser.ParseComments)
+	if err != nil {
+		return "", err
+	}
+
+	// Remove comments
+	ast.Inspect(file, func(n ast.Node) bool {
+		if _, ok := n.(*ast.CommentGroup); ok {
+			return false
+		}
+		return true
+	})
+
+	// Aggressive minification for higher levels
+	if level > 1 {
+		ast.Inspect(file, func(n ast.Node) bool {
+			switch x := n.(type) {
+			case *ast.Ident:
+				// Shorten identifiers (risky, only for very high minification)
+				if len(x.Name) > 3 && level > 2 {
+					x.Name = x.Name[:3]
+				}
+			case *ast.GenDecl:
+				// Combine var declarations
+				if x.Tok == token.VAR && len(x.Specs) > 1 {
+					for i := 1; i < len(x.Specs); i++ {
+						x.Specs[0].(*ast.ValueSpec).Names = append(x.Specs[0].(*ast.ValueSpec).Names, x.Specs[i].(*ast.ValueSpec).Names...)
+						x.Specs[0].(*ast.ValueSpec).Values = append(x.Specs[0].(*ast.ValueSpec).Values, x.Specs[i].(*ast.ValueSpec).Values...)
+					}
+					x.Specs = x.Specs[:1]
+				}
+			}
+			return true
+		})
+	}
+
+	// Merge import declarations
+	var importSpecs []ast.Spec
+	for _, decl := range file.Decls {
+		if genDecl, ok := decl.(*ast.GenDecl); ok && genDecl.Tok == token.IMPORT {
+			importSpecs = append(importSpecs, genDecl.Specs...)
+		}
+	}
+	if len(importSpecs) > 0 {
+		file.Decls = append([]ast.Decl{&ast.GenDecl{Tok: token.IMPORT, Specs: importSpecs}}, file.Decls...)
+	}
+
+	var buf bytes.Buffer
+	err = printer.Fprint(&buf, fset, file)
+	if err != nil {
+		return "", err
+	}
+	minified := buf.String()
+
+	// Remove unnecessary whitespace
+	minified = regexp.MustCompile(`\s+`).ReplaceAllString(minified, " ")
+	minified = regexp.MustCompile(`\{\s+`).ReplaceAllString(minified, "{")
+	minified = regexp.MustCompile(`\s+\}`).ReplaceAllString(minified, "}")
+	minified = regexp.MustCompile(`;\s*`).ReplaceAllString(minified, ";")
+
+	return minified, nil
+}
+
+func collectFiles(dir string, config Config) (BundleMeta, map[string]string, error) {
+	meta := BundleMeta{
+		S: make(map[string][]string),
+	}
+	files := make(map[string]string)
+	var allImports []string
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, _ := filepath.Rel(dir, path)
+
+		for _, excludeDir := range config.ExcludeDirs {
+			if strings.HasPrefix(relPath, excludeDir) {
+				if info.IsDir() {
+					return filepath.SkipDir
+				}
+				return nil
+			}
+		}
+
+		if !info.IsDir() && strings.HasSuffix(path, ".go") {
+			content, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			minified, err := minifyGoCode(string(content), config.MinifyLevel)
+			if err != nil {
+				return err
+			}
+
+			files[relPath] = minified
+
+			// Extract package and imports
+			fset := token.NewFileSet()
+			f, err := parser.ParseFile(fset, "", minified, parser.ImportsOnly)
+			if err == nil {
+				pkg := f.Name.Name
+				meta.S[pkg] = append(meta.S[pkg], relPath)
+				for _, imp := range f.Imports {
+					impPath := strings.Trim(imp.Path.Value, "\"")
+					if !contains(allImports, impPath) {
+						allImports = append(allImports, impPath)
+					}
+				}
+			}
+		}
+
+		return nil
+	})
+
+	meta.I = allImports
+	return meta, files, err
+}
+
+func readGoMod(filename string) ([]string, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := modfile.Parse(filename, content, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var dependencies []string
+	for _, req := range f.Require {
+		dependencies = append(dependencies, fmt.Sprintf("%s@%s", req.Mod.Path, req.Mod.Version))
+	}
+
+	return dependencies, nil
+}
+
+func createProjectBundle(config Config) error {
+	meta, files, err := collectFiles(config.ProjectDir, config)
+	if err != nil {
+		return fmt.Errorf("error collecting files: %v", err)
+	}
+
+	dependencies, err := readGoMod(filepath.Join(config.ProjectDir, "go.mod"))
+	if err != nil {
+		return fmt.Errorf("error reading go.mod: %v", err)
+	}
+	meta.D = dependencies
+
+	if !config.IncludeMeta {
+		meta.S = nil // Remove structure info if not needed
+	}
+
+	metaJSON, err := json.Marshal(meta)
+	if err != nil {
+		return fmt.Errorf("error creating JSON: %v", err)
+	}
+
+	output := string(metaJSON) + "\n"
+
+	for path, content := range files {
+		output += fmt.Sprintf("###FILE:%s###\n%s\n", path, content)
+	}
+
+	err = os.WriteFile(config.OutputFile, []byte(output), 0644)
+	if err != nil {
+		return fmt.Errorf("error writing to file: %v", err)
+	}
+
+	fmt.Printf("AI-friendly Go project bundle created: %s\n", config.OutputFile)
+	return nil
+}
+
+func contains(slice []string, item string) bool {
+	for _, a := range slice {
+		if a == item {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	config := Config{}
+
+	// Get the base name of the project directory
+	defaultDir := "."
+	defaultProjectName := filepath.Base(defaultDir)
+	// Sanitize the project name for use in a filename
+	defaultProjectName = regexp.MustCompile(`[^a-zA-Z0-9_-]`).ReplaceAllString(defaultProjectName, "_")
+	defaultOutputFile := fmt.Sprintf("%s_bundle.txt", defaultProjectName)
+
+	flag.StringVar(&config.ProjectDir, "dir", defaultDir, "The root directory of the Go project")
+	flag.StringVar(&config.OutputFile, "out", defaultOutputFile, "The output file")
+	flag.BoolVar(&config.IncludeMeta, "meta", false, "Include metadata (package structure)")
+	flag.IntVar(&config.MinifyLevel, "minify", 1, "Minification level (1-3)")
+	excludeDirs := flag.String("exclude", "vendor,testdata", "Comma-separated list of directories to exclude")
+
+	flag.Parse()
+
+	// Update the output filename if a custom project directory is specified
+	if config.ProjectDir != defaultDir && config.OutputFile == defaultOutputFile {
+		projectName := filepath.Base(config.ProjectDir)
+		projectName = regexp.MustCompile(`[^a-zA-Z0-9_-]`).ReplaceAllString(projectName, "_")
+		config.OutputFile = fmt.Sprintf("%s_bundle.txt", projectName)
+	}
+
+	config.ExcludeDirs = strings.Split(*excludeDirs, ",")
+
+	if err := createProjectBundle(config); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
